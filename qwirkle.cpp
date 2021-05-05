2a1921accd4c934593a966fe494e6abda496abd9
Lachlan Stavrevski <s3843294@student.rmit.edu.au>
2021-05-05 14:58:10 +1000

    Menu error checking

--- qwirkle.cpp
+++ qwirkle.cpp
@@ -9,21 +9,27 @@ void welcomeMessage();
 void showMenu();
 void getInput(std::string &input);
 void makeSelection(std::string);
+
+bool inputCorrect = false;
+
 int main(void)
-{
-   // std::string input;
-   // welcomeMessage();
-   // showMenu();
-   // getInput(input);
-   // makeSelection(input);
+{  
+   std::string input;
+   welcomeMessage();
+   showMenu();
+   while (!inputCorrect)
+   {
+      getInput(input);
+      makeSelection(input);
+   }
 
    // LinkedList *list = new LinkedList();
    // delete list;
 
    // std::cout << "TODO: Implement Qwirkle!" << std::endl;
 
-   TileBag* tilebag = new TileBag();
-   std::cout << tilebag << std::endl;
+   // TileBag* tilebag = new TileBag();
+   // std::cout << tilebag << std::endl;
 
    return EXIT_SUCCESS;
 }
@@ -53,21 +59,46 @@ void getInput(std::string &input)
 
 void makeSelection(std::string input)
 {
-   if (input.compare("1") == 0)
+   if ((input == "1") || (input == "2") || (input == "3") || (input == "4"))
    {
-      std::cout << "New Game selected" << std::endl;
+      if (input.compare("1") == 0)
+      {
+         std::cout << "New Game selected" << std::endl;
+         inputCorrect = true;
+      }
+      else if (input.compare("2") == 0)
+      {
+         std::cout << "Load Game selected" << std::endl;
+         inputCorrect = true;
+      }
+      else if (input.compare("3") == 0)
+      {
+         std::cout << "Name: Yonas Sisay" << std::endl;
+         std::cout << "Student ID: s3659939" << std::endl;
+         std::cout << "Email: s3659939@student.rmit.edu.au" << std::endl;
+         std::cout << "" << std::endl;
+         std::cout << "Name: Lachlan Kris Stavrevski" << std::endl;
+         std::cout << "Student ID: s3843294" << std::endl;
+         std::cout << "Email: s3843294@student.rmit.edu.au" << std::endl;
+         std::cout << "" << std::endl;
+         std::cout << "Name: Roman O'Brien" << std::endl;
+         std::cout << "Student ID: s3845295" << std::endl;
+         std::cout << "Email: s3845295@student.rmit.edu.au" << std::endl;
+         std::cout << "" << std::endl;
+         std::cout << "Name: David Morrin" << std::endl;
+         std::cout << "Student ID: s3845295" << std::endl;
+         std::cout << "Email: s3845230@student.rmit.edu.au" << std::endl;
+         showMenu();
+      }
+      else if (input.compare("4") == 0)
+      {
+         std::cout << "" << std::endl;
+         std::cout << "Goodbye" << std::endl;
+         inputCorrect = true;
+      }  
    }
-   else if (input.compare("2") == 0)
+   else
    {
-      std::cout << "Load Game selected" << std::endl;
-   }
-   else if (input.compare("3") == 0)
-   {
-      std::cout << "Credits selected" << std::endl;
-   }
-   else if (input.compare("4") == 0)
-   {
-
-      std::cout << "Quit selected" << std::endl;
+      std::cout << "Please enter a valid option" << std::endl;
    }
 }
