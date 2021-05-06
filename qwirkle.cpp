68b379388b0285834ac74899b258e20e9727980d
[Roman O'Brien s3845295] <s3845295@student.rmit.edu.au>
2021-05-06 19:29:25 +1000

    Commented out test lines in qwirkle.cpp

--- qwirkle.cpp
+++ qwirkle.cpp
@@ -26,19 +26,18 @@ int main(void)
    // TileBag* tilebag = new TileBag();
    // std::cout << tilebag << std::endl;
 
-   Board* board = new Board();
-   std::cout << board << std::endl;
+   // Board* board = new Board();
+   // std::cout << board << std::endl;
    
-
-   std::cout << board->scoreValidate(1,1, new Tile('R', 1)) << std::endl;
-   std::cout << board->scoreValidate(1,2, new Tile('R', 2)) << std::endl;
-   std::cout << board->scoreValidate(1,3, new Tile('R', 3)) << std::endl;
-   std::cout << board->scoreValidate(1,4, new Tile('R', 4)) << std::endl;
-   std::cout << board->scoreValidate(1,5, new Tile('R', 5)) << std::endl;
-   std::cout << board->scoreValidate(1,6, new Tile('R', 6)) << std::endl;
-   std::cout << board->scoreValidate(1,7, new Tile('R', 7)) << std::endl;
-   std::cout << board->scoreValidate(2,6, new Tile('R', 5)) << std::endl;
-   std::cout << board->scoreValidate(2,5, new Tile('G', 5)) << std::endl;
+   // std::cout << board->scoreValidate(1,1, new Tile('R', 1)) << std::endl;
+   // std::cout << board->scoreValidate(1,2, new Tile('R', 2)) << std::endl;
+   // std::cout << board->scoreValidate(1,3, new Tile('R', 3)) << std::endl;
+   // std::cout << board->scoreValidate(1,4, new Tile('R', 4)) << std::endl;
+   // std::cout << board->scoreValidate(1,5, new Tile('R', 5)) << std::endl;
+   // std::cout << board->scoreValidate(1,6, new Tile('R', 6)) << std::endl;
+   // std::cout << board->scoreValidate(1,7, new Tile('R', 7)) << std::endl;
+   // std::cout << board->scoreValidate(2,6, new Tile('R', 5)) << std::endl;
+   // std::cout << board->scoreValidate(2,5, new Tile('G', 5)) << std::endl;
 
 
    return EXIT_SUCCESS;
